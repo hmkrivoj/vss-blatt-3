c85cb33b1293aa8e78ea67bc802e9bbec2855def
hmkrivoj <krivoj@hm.edu>
2019-04-16 12:39:50 +0200

    created node actor

--- tree/node.go
+++ tree/node.go
@@ -1,1 +1,14 @@
 package tree
+
+import (
+	"github.com/AsynkronIT/protoactor-go/actor"
+)
+
+type NodeActor struct {
+	left, right NodeActor
+	content     map[int]string
+}
+
+func (state *NodeActor) Receive(context actor.Context) {
+
+}
