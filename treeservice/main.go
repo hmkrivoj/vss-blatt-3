aade592e574c6f42d05bb0344c611b8aa7825af1
hmkrivoj <krivoj@hm.edu>
2019-05-02 17:23:23 +0200

    respecified messages

--- treeservice/main.go
+++ treeservice/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/remote"
+	"github.com/urfave/cli"
+	"os"
 )
 
+type treeserviceActor struct {
+	tokens map[int64]string
+	trees  map[int64]actor.PID
+}
+
+func (*treeserviceActor) Receive(c actor.Context) {
+	panic("implement me")
+}
+
+func newTreeserviceActor() actor.Actor {
+	return &treeserviceActor{}
+}
+
+func init() {
+	remote.Register("treeservice", actor.PropsFromProducer(newTreeserviceActor))
+}
+
 func main() {
-	fmt.Println("Hello Tree-Service!")
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "bind",
+			Usage: "the address treeservice shall bind to",
+			Value: "treeservice.actors:8090",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		remote.Start(c.String("bind"))
+		return nil
+	}
+	_ = app.Run(os.Args)
 }
