b7b85fef4cc41a56fecde3f8fd0f11997616dc27
hmkrivoj <krivoj@hm.edu>
2019-05-03 14:43:41 +0200

    barebone treeservice

--- treeservice/main.go
+++ treeservice/main.go
@@ -1,27 +1,62 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
 	"github.com/AsynkronIT/protoactor-go/actor"
 	"github.com/AsynkronIT/protoactor-go/remote"
+	"github.com/ob-vss-ss19/blatt-3-forever_alone/messages"
+	"github.com/ob-vss-ss19/blatt-3-forever_alone/tree"
 	"github.com/urfave/cli"
 	"os"
 )
 
-type treeserviceActor struct {
-	tokens map[int64]string
-	trees  map[int64]actor.PID
+type treeServiceActor struct {
+	tokens    map[int64]string
+	trees     map[int64]*actor.PID
+	idCounter int64
 }
 
-func (*treeserviceActor) Receive(c actor.Context) {
-	panic("implement me")
+func (state *treeServiceActor) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case messages.CreateTreeRequest:
+		id := state.idCounter
+		state.idCounter++
+		token := make([]byte, 4)
+		_, _ = rand.Read(token)
+
+		state.tokens[id] = fmt.Sprintf("%x", token)
+		state.trees[id] = context.Spawn(actor.PropsFromProducer(tree.NodeActorProducer))
+
+		context.Send(state.trees[id], messages.CreateTreeRequest{MaxSize: msg.MaxSize})
+		context.Respond(
+			messages.CreateTreeResponse{Credentials: &messages.Credentials{Id: id, Token: state.tokens[id]}},
+		)
+	case messages.SearchRequest:
+		if state.tokens[msg.Credentials.Id] != msg.Credentials.Token {
+			context.Respond(messages.SearchResponse{Key: msg.Key, Type: messages.ACCESS_DENIED})
+		} else {
+			context.Forward(state.trees[msg.Credentials.Id])
+		}
+	case messages.InsertRequest:
+		if state.tokens[msg.Credentials.Id] != msg.Credentials.Token {
+			context.Respond(messages.InsertResponse{Key: msg.Key, Type: messages.ACCESS_DENIED})
+		} else {
+			context.Forward(state.trees[msg.Credentials.Id])
+		}
+	}
 }
 
-func newTreeserviceActor() actor.Actor {
-	return &treeserviceActor{}
+func newTreeServiceActor() actor.Actor {
+	myActor := treeServiceActor{}
+	myActor.idCounter = 1
+	myActor.tokens = make(map[int64]string)
+	myActor.trees = make(map[int64]*actor.PID)
+	return &myActor
 }
 
 func init() {
-	remote.Register("treeservice", actor.PropsFromProducer(newTreeserviceActor))
+	remote.Register("treeservice", actor.PropsFromProducer(newTreeServiceActor))
 }
 
 func main() {
