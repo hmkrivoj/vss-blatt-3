3ebcd4c95a3e53a753ab7a6430c132105d646aa6
hmkrivoj <krivoj@hm.edu>
2019-05-04 22:38:56 +0200

    extracted requestResult and added checks for wrong types

--- treecli/main.go
+++ treecli/main.go
@@ -49,18 +49,23 @@ func handleCredentialsFromCliContext(c *cli.Context) {
 	}
 }
 
-func commandCreatetreeAction(c *cli.Context) error {
-	pid := spawnRemoteFromCliContext(c)
-	res, err := actor.EmptyRootContext.RequestFuture(
-		pid,
-		&messages.CreateTreeRequest{MaxSize: c.Int64(commandCreatetreeFlagMaxsize)},
-		timeout,
-	).Result()
+func requestResult(pid *actor.PID, message interface{}) interface{} {
+	result, err := actor.EmptyRootContext.RequestFuture(pid, message, timeout).Result()
 	if err != nil {
 		panic(err)
 	}
-	response := res.(*messages.CreateTreeResponse)
-	fmt.Printf("id: %d, token: %s\n", response.Credentials.Id, response.Credentials.Token)
+	return result
+}
+
+func commandCreatetreeAction(c *cli.Context) error {
+	pid := spawnRemoteFromCliContext(c)
+	res := requestResult(pid, &messages.CreateTreeRequest{MaxSize: c.Int64(commandCreatetreeFlagMaxsize)})
+	switch response := res.(type) {
+	case *messages.CreateTreeResponse:
+		fmt.Printf("id: %d, token: %s\n", response.Credentials.Id, response.Credentials.Token)
+	default:
+		panic("Wrong message type")
+	}
 	return nil
 }
 
@@ -70,7 +75,7 @@ func commandInsertAction(c *cli.Context) error {
 	}
 	handleCredentialsFromCliContext(c)
 	pid := spawnRemoteFromCliContext(c)
-	res, err := actor.EmptyRootContext.RequestFuture(
+	res := requestResult(
 		pid,
 		&messages.InsertRequest{
 			Credentials: &messages.Credentials{
@@ -80,23 +85,23 @@ func commandInsertAction(c *cli.Context) error {
 			Key:   c.Int64(commandInsertFlagKey),
 			Value: c.String(commandInsertFlagValue),
 		},
-		timeout,
-	).Result()
-	if err != nil {
-		panic(err)
-	}
-	response := res.(*messages.InsertResponse)
-	switch response.Type {
-	case messages.SUCCESS:
-		fmt.Printf("(%d, %s) successfully inserted\n", c.Int64(commandInsertFlagKey), c.String(commandInsertFlagValue))
-	case messages.KEY_ALREADY_EXISTS:
-		panic(fmt.Sprintf("Tree already contains key %d", c.Int64(commandInsertFlagKey)))
-	case messages.ACCESS_DENIED:
-		panic("Invalid credentials")
-	case messages.NO_SUCH_TREE:
-		panic("No such tree")
+	)
+	switch msg := res.(type) {
+	case *messages.InsertResponse:
+		switch msg.Type {
+		case messages.SUCCESS:
+			fmt.Printf("(%d, %s) successfully inserted\n", c.Int64(commandInsertFlagKey), c.String(commandInsertFlagValue))
+		case messages.KEY_ALREADY_EXISTS:
+			panic(fmt.Sprintf("Tree already contains key %d", c.Int64(commandInsertFlagKey)))
+		case messages.ACCESS_DENIED:
+			panic("Invalid credentials")
+		case messages.NO_SUCH_TREE:
+			panic("No such tree")
+		default:
+			panic("Unknown response type")
+		}
 	default:
-		panic("Unknown response type")
+		panic("Wrong message type")
 	}
 	return nil
 }
@@ -107,7 +112,7 @@ func commandSearchAction(c *cli.Context) error {
 	}
 	handleCredentialsFromCliContext(c)
 	pid := spawnRemoteFromCliContext(c)
-	res, err := actor.EmptyRootContext.RequestFuture(
+	res := requestResult(
 		pid,
 		&messages.SearchRequest{
 			Credentials: &messages.Credentials{
@@ -116,23 +121,23 @@ func commandSearchAction(c *cli.Context) error {
 			},
 			Key: c.Int64(commandSearchFlagKey),
 		},
-		timeout,
-	).Result()
-	if err != nil {
-		panic(err)
-	}
-	response := res.(*messages.SearchResponse)
-	switch response.Type {
-	case messages.SUCCESS:
-		fmt.Printf("Value for key %d: %s\n", c.Int64(commandSearchFlagKey), response.Value)
-	case messages.NO_SUCH_KEY:
-		panic(fmt.Sprintf("Tree contains no key %d", c.Int64(commandSearchFlagKey)))
-	case messages.ACCESS_DENIED:
-		panic("Invalid credentials")
-	case messages.NO_SUCH_TREE:
-		panic("No such tree")
+	)
+	switch msg := res.(type) {
+	case *messages.SearchResponse:
+		switch msg.Type {
+		case messages.SUCCESS:
+			fmt.Printf("Value for key %d: %s\n", c.Int64(commandSearchFlagKey), msg.Value)
+		case messages.NO_SUCH_KEY:
+			panic(fmt.Sprintf("Tree contains no key %d", c.Int64(commandSearchFlagKey)))
+		case messages.ACCESS_DENIED:
+			panic("Invalid credentials")
+		case messages.NO_SUCH_TREE:
+			panic("No such tree")
+		default:
+			panic("Unknown response type")
+		}
 	default:
-		panic("Unknown response type")
+		panic("Wrong message type")
 	}
 	return nil
 }
