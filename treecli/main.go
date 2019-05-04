ff43893133a2d9ed78440d90ad73e49cea19fead
Dimitri Krivoj <32704196+hmkrivoj@users.noreply.github.com>
2019-05-04 23:41:15 +0200

    Refactoring (#4)
    * extracted constants in cli
    
    * extracted anonymous functions
    
    * renamed stuff and extracted more constants
    
    * extracted spawnRemoteFromCliContext
    
    * extracted handleCredentialsFromCliContext
    
    * reordered consts
    
    * extracted requestResult and added checks for wrong types
    
    * capitalized id for linter

--- treecli/main.go
+++ treecli/main.go
@@ -11,177 +11,191 @@ import (
 	"github.com/urfave/cli"
 )
 
+const timeout = 5 * time.Second
+
+const globalFlagBind = "bind"
+const globalFlagRemote = "remote"
+const globalFlagID = "id"
+const globalFlagToken = "token"
+
+const commandCreatetreeName = "createtree"
+const commandCreatetreeFlagMaxsize = "maxsize"
+
+const commandInsertName = "insert"
+const commandInsertFlagKey = "key"
+const commandInsertFlagValue = "value"
+
+const commandSearchName = "search"
+const commandSearchFlagKey = "key"
+
+func spawnRemoteFromCliContext(c *cli.Context) *actor.PID {
+	remote.Start(c.GlobalString(globalFlagBind))
+	pidResp, err := remote.SpawnNamed(
+		c.GlobalString(globalFlagRemote),
+		"remote",
+		"treeservice",
+		timeout,
+	)
+	if err != nil {
+		panic(err)
+	}
+	pid := pidResp.Pid
+	return pid
+}
+
+func handleCredentialsFromCliContext(c *cli.Context) {
+	if !c.GlobalIsSet(globalFlagID) || !c.GlobalIsSet(globalFlagToken) {
+		panic("Missing credentials.")
+	}
+}
+
+func requestResult(pid *actor.PID, message interface{}) interface{} {
+	result, err := actor.EmptyRootContext.RequestFuture(pid, message, timeout).Result()
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func commandCreatetreeAction(c *cli.Context) error {
+	pid := spawnRemoteFromCliContext(c)
+	res := requestResult(pid, &messages.CreateTreeRequest{MaxSize: c.Int64(commandCreatetreeFlagMaxsize)})
+	switch response := res.(type) {
+	case *messages.CreateTreeResponse:
+		fmt.Printf("id: %d, token: %s\n", response.Credentials.Id, response.Credentials.Token)
+	default:
+		panic("Wrong message type")
+	}
+	return nil
+}
+
+func commandInsertAction(c *cli.Context) error {
+	if !c.IsSet(commandInsertFlagKey) || !c.IsSet(commandInsertFlagValue) {
+		panic("Missing key or value.")
+	}
+	handleCredentialsFromCliContext(c)
+	pid := spawnRemoteFromCliContext(c)
+	res := requestResult(
+		pid,
+		&messages.InsertRequest{
+			Credentials: &messages.Credentials{
+				Token: c.GlobalString(globalFlagToken),
+				Id:    c.GlobalInt64(globalFlagID),
+			},
+			Key:   c.Int64(commandInsertFlagKey),
+			Value: c.String(commandInsertFlagValue),
+		},
+	)
+	switch msg := res.(type) {
+	case *messages.InsertResponse:
+		switch msg.Type {
+		case messages.SUCCESS:
+			fmt.Printf("(%d, %s) successfully inserted\n", c.Int64(commandInsertFlagKey), c.String(commandInsertFlagValue))
+		case messages.KEY_ALREADY_EXISTS:
+			panic(fmt.Sprintf("Tree already contains key %d", c.Int64(commandInsertFlagKey)))
+		case messages.ACCESS_DENIED:
+			panic("Invalid credentials")
+		case messages.NO_SUCH_TREE:
+			panic("No such tree")
+		default:
+			panic("Unknown response type")
+		}
+	default:
+		panic("Wrong message type")
+	}
+	return nil
+}
+
+func commandSearchAction(c *cli.Context) error {
+	if !c.IsSet(commandSearchFlagKey) {
+		panic("Missing key.")
+	}
+	handleCredentialsFromCliContext(c)
+	pid := spawnRemoteFromCliContext(c)
+	res := requestResult(
+		pid,
+		&messages.SearchRequest{
+			Credentials: &messages.Credentials{
+				Token: c.GlobalString(globalFlagToken),
+				Id:    c.GlobalInt64(globalFlagID),
+			},
+			Key: c.Int64(commandSearchFlagKey),
+		},
+	)
+	switch msg := res.(type) {
+	case *messages.SearchResponse:
+		switch msg.Type {
+		case messages.SUCCESS:
+			fmt.Printf("Value for key %d: %s\n", c.Int64(commandSearchFlagKey), msg.Value)
+		case messages.NO_SUCH_KEY:
+			panic(fmt.Sprintf("Tree contains no key %d", c.Int64(commandSearchFlagKey)))
+		case messages.ACCESS_DENIED:
+			panic("Invalid credentials")
+		case messages.NO_SUCH_TREE:
+			panic("No such tree")
+		default:
+			panic("Unknown response type")
+		}
+	default:
+		panic("Wrong message type")
+	}
+	return nil
+}
+
 func main() {
-	timeout := 5 * time.Second
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "bind",
+			Name:  globalFlagBind,
 			Usage: "address treecli should use",
 			Value: "treecli.actors:8091",
 		},
 		cli.StringFlag{
-			Name:  "remote",
+			Name:  globalFlagRemote,
 			Usage: "address of the treeservice",
 			Value: "treeservice.actors:8090",
 		},
 		cli.Int64Flag{
-			Name:  "id",
+			Name:  globalFlagID,
 			Usage: "id of the tree you want to alter",
 		},
 		cli.StringFlag{
-			Name:  "token",
+			Name:  globalFlagToken,
 			Usage: "token to authorize your access for the specified tree",
 		},
 	}
 	app.Commands = []cli.Command{
 		{
-			Name: "createtree",
+			Name: commandCreatetreeName,
 			Flags: []cli.Flag{
 				cli.Int64Flag{
-					Name:  "maxsize",
+					Name:  commandCreatetreeFlagMaxsize,
 					Usage: "max size of a leaf",
 					Value: 2,
 				},
 			},
-			Action: func(c *cli.Context) error {
-				remote.Start(c.GlobalString("bind"))
-				pidResp, err := remote.SpawnNamed(
-					c.GlobalString("remote"),
-					"remote",
-					"treeservice",
-					timeout,
-				)
-				if err != nil {
-					panic(err)
-				}
-				pid := pidResp.Pid
-				res, err := actor.EmptyRootContext.RequestFuture(
-					pid,
-					&messages.CreateTreeRequest{MaxSize: c.Int64("maxsize")},
-					timeout,
-				).Result()
-				if err != nil {
-					panic(err)
-				}
-				response := res.(*messages.CreateTreeResponse)
-				fmt.Printf("id: %d, token: %s\n", response.Credentials.Id, response.Credentials.Token)
-				return nil
-			},
+			Action: commandCreatetreeAction,
 		},
 		{
-			Name: "insert",
+			Name: commandInsertName,
 			Flags: []cli.Flag{
 				cli.Int64Flag{
-					Name: "key",
+					Name: commandInsertFlagKey,
 				},
 				cli.StringFlag{
-					Name: "value",
+					Name: commandInsertFlagValue,
 				},
 			},
-			Action: func(c *cli.Context) error {
-				if !c.IsSet("key") || !c.IsSet("value") {
-					panic("Missing key or value.")
-				}
-				if !c.GlobalIsSet("id") || !c.GlobalIsSet("token") {
-					panic("Missing credentials.")
-				}
-				remote.Start(c.GlobalString("bind"))
-				pidResp, err := remote.SpawnNamed(
-					c.GlobalString("remote"),
-					"remote",
-					"treeservice",
-					timeout,
-				)
-				if err != nil {
-					panic(err)
-				}
-				pid := pidResp.Pid
-				res, err := actor.EmptyRootContext.RequestFuture(
-					pid,
-					&messages.InsertRequest{
-						Credentials: &messages.Credentials{
-							Token: c.GlobalString("token"),
-							Id:    c.GlobalInt64("id"),
-						},
-						Key:   c.Int64("key"),
-						Value: c.String("value"),
-					},
-					timeout,
-				).Result()
-				if err != nil {
-					panic(err)
-				}
-				response := res.(*messages.InsertResponse)
-				switch response.Type {
-				case messages.SUCCESS:
-					fmt.Printf("(%d, %s) successfully inserted\n", c.Int64("key"), c.String("value"))
-				case messages.KEY_ALREADY_EXISTS:
-					panic(fmt.Sprintf("Tree already contains key %d", c.Int64("key")))
-				case messages.ACCESS_DENIED:
-					panic("Invalid credentials")
-				case messages.NO_SUCH_TREE:
-					panic("No such tree")
-				default:
-					panic("Unknown response type")
-				}
-				return nil
-			},
+			Action: commandInsertAction,
 		},
 		{
-			Name: "search",
+			Name: commandSearchName,
 			Flags: []cli.Flag{
 				cli.Int64Flag{
-					Name: "key",
+					Name: commandSearchFlagKey,
 				},
 			},
-			Action: func(c *cli.Context) error {
-				if !c.IsSet("key") {
-					panic("Missing key.")
-				}
-				if !c.GlobalIsSet("id") || !c.GlobalIsSet("token") {
-					panic("Missing credentials.")
-				}
-				remote.Start(c.GlobalString("bind"))
-				pidResp, err := remote.SpawnNamed(
-					c.GlobalString("remote"),
-					"remote",
-					"treeservice",
-					timeout,
-				)
-				if err != nil {
-					panic(err)
-				}
-				pid := pidResp.Pid
-				res, err := actor.EmptyRootContext.RequestFuture(
-					pid,
-					&messages.SearchRequest{
-						Credentials: &messages.Credentials{
-							Token: c.GlobalString("token"),
-							Id:    c.GlobalInt64("id"),
-						},
-						Key: c.Int64("key"),
-					},
-					timeout,
-				).Result()
-				if err != nil {
-					panic(err)
-				}
-				response := res.(*messages.SearchResponse)
-				switch response.Type {
-				case messages.SUCCESS:
-					fmt.Printf("Value for key %d: %s\n", c.Int64("key"), response.Value)
-				case messages.NO_SUCH_KEY:
-					panic(fmt.Sprintf("Tree contains no key %d", c.Int64("key")))
-				case messages.ACCESS_DENIED:
-					panic("Invalid credentials")
-				case messages.NO_SUCH_TREE:
-					panic("No such tree")
-				default:
-					panic("Unknown response type")
-				}
-				return nil
-			},
+			Action: commandSearchAction,
 		},
 	}
 	_ = app.Run(os.Args)
